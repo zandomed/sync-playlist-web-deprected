880e10e10b74b6130eb2a46bc2caaf928c4403a3
Miguel Mendoza <miguel@zandome.dev>
2025-08-22 18:48:48 +0200

    feat: add ESLint, Prettier, and lint-staged configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: true,
+    viewTransition: true,
+  },
+  // typedRoutes: true,
 };
 
 export default nextConfig;
